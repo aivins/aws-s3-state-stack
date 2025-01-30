b5ad247f76b540a7a10f9deed55349b6cc15a66a
Andrew Ivins <andrew@ivins.id.au>
2025-01-30 23:09:43 +0800

    work

--- src/aws_s3_state_stack/settings.py
+++ src/aws_s3_state_stack/settings.py
@@ -1,117 +1,203 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Dict, Type, TypedDict
+from typing import Any, Dict, Tuple, Type, TypedDict
 
 import boto3
-from pydantic import BaseModel
+import botocore.client
+from pydantic import BaseModel, PrivateAttr, computed_field
+from pydantic_settings import (
+    BaseSettings,
+    InitSettingsSource,
+    PydanticBaseSettingsSource,
+    SettingsConfigDict,
+)
+
+# @dataclass
+# class Setting:
+#     namespace: str
+#     key: str
+#     value: str
+#     description: str = ""
+#     required: bool = False
+
+#     def __str__(self) -> str:
+#         return self.value
 
 
-@dataclass
-class Setting:
-    namespace: str
+class Setting(BaseModel):
     key: str
     value: str
     description: str = ""
-    required: bool = False
-
-    def __str__(self) -> str:
-        return self.value
 
 
-class Settings(BaseModel):
-    class Config:
-        extra = "allow"
+class ParameterStoreSettingsSource(PydanticBaseSettingsSource):
+    def get_field_value(self, field, field_name):
+        breakpoint()
+
+    def __call__(self):
+        session = self.settings_sources_data["InitSettingsSource"]["_session"]
+        ssm = session.client("ssm")
+        namespace = self.settings_sources_data["InitSettingsSource"]["namespace"]
+        response = ssm.describe_parameters(
+            ParameterFilters=[
+                {
+                    "Key": "Name",
+                    "Option": "BeginsWith",
+                    "Values": [
+                        namespace,
+                    ],
+                },
+            ]
+        )
+        descriptions = {p["Name"]: p["Description"] for p in response["Parameters"]}
+        response = ssm.get_parameters_by_path(Path=namespace, Recursive=True)
+        params = response.get("Parameters", [])
+        settings = {}
+        for param in params:
+            description = descriptions[param["Name"]]
+            key = param["Name"][len(namespace) + 1 :]
+            if key in self.settings_cls.model_fields:
+                settings[key] = Setting(
+                    key=key, value=param["Value"], description=description
+                )
+        return settings
 
 
-class AppSettings(ABC):
-    def __init__(self, app: str, environment: str, settings_type: type = Settings):
-        self.app = app
-        self.environment = environment
-        self._settings_type = settings_type
-        self._settings = settings_type.model_construct()
+class AppSettings(BaseSettings):
+    model_config = {
+        "extra": "allow",
+    }
+    app: str
+    environment: str
+    namespace: str
 
-    @property
-    @abstractmethod
-    def namespace(self) -> str:
-        pass
+    def __init__(self, app: str, environment: str, **kwargs):
+        namespace = f"/{app}/{environment}"
+        super().__init__(
+            namespace=namespace,
+            app=app,
+            environment=environment,
+            **kwargs,
+        )
+        # self._namespace = namespace
 
-    @abstractmethod
-    def _get(self, key: str) -> Setting:
-        pass
+    # def _get(self, key: str) -> Setting:
+    #     return getattr(self, key)
 
-    @abstractmethod
-    def _set(self, key: str, value: str) -> None:
-        pass
+    # def _set(self, key: str, **data):
+    #     return setattr(self, key, data)
 
-    @abstractmethod
-    def _get_all(self) -> Settings:
-        pass
+    # def get(self, key: str) -> Setting:
+    #     return self._get(key)
 
-    @abstractmethod
-    def _save(self) -> None:
-        pass
+    # def set(self, key: str, **data) -> None:
+    #     self._set(key, **data)
 
-    def get(self, key: str) -> Setting:
-        return self._get(key)
+    # def __getattr__(self, key: str) -> Any:
+    #     if key in self.model_fields.keys():
+    #         return super().__getattr__(key)
+    #     return super().__getattr__(key)
 
-    def set(self, key: str) -> Setting:
-        return self._set(key)
+    # def __setattr__(self, key: str, value: Any) -> Any:
+    #     if key in self.model_fields.keys():
+    #         return super().__setattr__(key, value)
+    #     return super().__setattr__(key, value)
 
-    def get_all(self) -> Settings:
-        return self._get_all()
+    # @abstractmethod
+    # def _get(self, key: str) -> Setting:
+    #     pass
 
-    def save(self) -> None:
-        self._save()
+    # @abstractmethod
+    # def _set(self, key: str, value: str) -> None:
+    #     pass
+
+    # @abstractmethod
+    # def _get_all(self) -> Settings:
+    #     pass
+
+    # @abstractmethod
+    # def _save(self) -> None:
+    #     pass
+
+    # def get(self, key: str) -> Setting:
+    #     return self._get(key)
+
+    # def set(self, key: str) -> Setting:
+    #     return self._set(key)
+
+    # def get_all(self) -> Settings:
+    #     return self._get_all()
+
+    # def save(self) -> None:
+    #     self._save()
 
 
 class AwsAppSettings(AppSettings):
+    _session: boto3.Session
+
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.session = boto3.Session()
-        self.ssm = self.session.client("ssm")
+        super().__init__(*args, _session=boto3.Session(), **kwargs)
 
-    @property
-    def namespace(self) -> str:
-        return f"/{self.app}/{self.environment}"
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: Type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> Tuple[PydanticBaseSettingsSource, ...]:
+        return (init_settings, ParameterStoreSettingsSource(settings_cls))
 
-    def full_key(self, key: str) -> str:
-        return f"{self.namespace}/{key}"
 
-    def setting_from_parameter(self, param: dict) -> Setting:
-        key: str = param["Name"][len(self.namespace) + 1 :]
-        return Setting(
-            self.namespace, key, param["Value"], param.get("Description", "")
-        )
+# class AwsAppSettings(AppSettings):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.session = boto3.Session()
+#         self.ssm = self.session.client("ssm")
 
-    def _get(self, key: str) -> Setting:
-        if key in self._settings:
-            return self._settings[key]
-        response = self.ssm.get_parameter(Name=self.full_key(key))
-        return self.setting_from_parameter(response["Parameter"])
+#     @property
+#     def namespace(self) -> str:
+#         return f"/{self.app}/{self.environment}"
 
-    def _get_all(self) -> Settings:
-        if self._settings:
-            return self._settings
-        response = self.ssm.get_parameters_by_path(Path=self.namespace, Recursive=True)
-        params = response.get("Parameters", [])
-        for param in params:
-            setting = self.setting_from_parameter(param)
-            self._settings[setting.key] = setting
-        return self._settings
+#     def full_key(self, key: str) -> str:
+#         return f"{self.namespace}/{key}"
 
-    def _set(self, key, value, description=""):
-        key = key.strip()
-        if not key:
-            raise Exception("Cannot set an empty key")
-        self._settings[key] = Setting(self.namespace, key, value, description)
+#     def setting_from_parameter(self, param: dict) -> Setting:
+#         key: str = param["Name"][len(self.namespace) + 1 :]
+#         return Setting(
+#             self.namespace, key, param["Value"], param.get("Description", "")
+#         )
 
-    def _save(self):
-        for key, setting in self._settings.items():
-            full_key = self.full_key(key)
-            self.ssm.put_parameter(
-                Type="String",
-                Name=full_key,
-                Value=setting.value,
-                Description=setting.description,
-                Overwrite=True,
-            )
+#     def _get(self, key: str) -> Setting:
+#         if key in self._settings:
+#             return self._settings[key]
+#         response = self.ssm.get_parameter(Name=self.full_key(key))
+#         return self.setting_from_parameter(response["Parameter"])
+
+#     def _get_all(self) -> Settings:
+#         if self._settings:
+#             return self._settings
+#         response = self.ssm.get_parameters_by_path(Path=self.namespace, Recursive=True)
+#         params = response.get("Parameters", [])
+#         for param in params:
+#             setting = self.setting_from_parameter(param)
+#             self._settings[setting.key] = setting
+#         return self._settings
+
+#     def _set(self, key, value, description=""):
+#         key = key.strip()
+#         if not key:
+#             raise Exception("Cannot set an empty key")
+#         self._settings[key] = Setting(self.namespace, key, value, description)
+
+#     def _save(self):
+#         for key, setting in self._settings.items():
+#             full_key = self.full_key(key)
+#             self.ssm.put_parameter(
+#                 Type="String",
+#                 Name=full_key,
+#                 Value=setting.value,
+#                 Description=setting.description,
+#                 Overwrite=True,
+#             )
--- tests/test_settings.py
+++ tests/test_settings.py
@@ -3,7 +3,7 @@ import pytest
 from moto import mock_aws
 from pydantic import BaseModel
 
-from aws_s3_state_stack.settings import AwsAppSettings, Setting, Settings
+from aws_s3_state_stack.settings import AwsAppSettings, Setting
 
 TEST_APP = "myapp"
 TEST_ENV = "dev"
@@ -25,60 +25,69 @@ def ssm():
 @pytest.fixture(scope="module")
 def settings(ssm):
     for key, value in TEST_PARAMS.items():
-        print(key, value)
-        ssm.put_parameter(Type="String", Name=key, Value=value)
-    yield AwsAppSettings(TEST_APP, TEST_ENV)
+        ssm.put_parameter(
+            Type="String", Name=key, Value=value, Description="Some description"
+        )
+
+    class TestSettings(AwsAppSettings):
+        foo: Setting
+        bar: Setting
+        other: Setting
+
+    yield TestSettings(app=TEST_APP, environment=TEST_ENV)
 
 
 def test_aws_namespace(settings):
-    assert settings.namespace == f"/{TEST_APP}/{TEST_ENV}"
-
-
-def test_fetch_all_settings(settings):
-    all_settings = settings.get_all()
-    assert len(all_settings) == 3
-    assert all_settings["foo"].value == "valueforfoo"
-    assert str(all_settings["foo"]) == "valueforfoo"
-
-
-def test_get_setting(settings):
-    assert settings.get("bar").value == "true"
-    assert str(settings.get("bar")) == "true"
-
-
-def test_set_setting(settings):
-    settings.set("hello", "there", "Just some setting")
-    setting = settings.get("hello")
-    setting.value == "there"
-    setting.description == "Just some setting"
-
-
-def test_set_bad_key(settings):
-    with pytest.raises(Exception, match="Cannot set an empty key"):
-        settings.set("", "blah")
-
-
-def test_save_settings(settings, ssm):
-    settings.set("new_key", "new_value")
-    settings.set("new_key1", "new_value2")
-    settings.save()
-    response = ssm.get_parameters_by_path(Path=settings.namespace, Recursive=True)
-    params = response["Parameters"]
-
-    def present(key):
-        return any(x["Name"] == settings.full_key(key) for x in params)
-
-    assert present("foo")
-    assert present("new_key")
-    assert present("new_key1")
-
-
-def test_typed_settings():
-    with mock_aws():
-
-        class Settings(BaseModel):
-            hostname: Setting
-            password: Setting
-
-    settings = AwsAppSettings(TEST_APP, TEST_ENV, settings_type=Settings)
     breakpoint()
+    assert settings.namespace == f"/{TEST_APP}/{TEST_ENV}"
+    breakpoint()
+
+
+# def test_fetch_all_settings(settings):
+#     all_settings = settings.get_all()
+#     assert len(all_settings) == 3
+#     assert all_settings["foo"].value == "valueforfoo"
+#     assert str(all_settings["foo"]) == "valueforfoo"
+
+
+# def test_get_setting(settings):
+#     assert settings.get("bar").value == "true"
+#     assert str(settings.get("bar")) == "true"
+
+
+# def test_set_setting(settings):
+#     settings.set("hello", "there", "Just some setting")
+#     setting = settings.get("hello")
+#     setting.value == "there"
+#     setting.description == "Just some setting"
+
+
+# def test_set_bad_key(settings):
+#     with pytest.raises(Exception, match="Cannot set an empty key"):
+#         settings.set("", "blah")
+
+
+# def test_save_settings(settings, ssm):
+#     settings.set("new_key", "new_value")
+#     settings.set("new_key1", "new_value2")
+#     settings.save()
+#     response = ssm.get_parameters_by_path(Path=settings.namespace, Recursive=True)
+#     params = response["Parameters"]
+
+#     def present(key):
+#         return any(x["Name"] == settings.full_key(key) for x in params)
+
+#     assert present("foo")
+#     assert present("new_key")
+#     assert present("new_key1")
+
+
+# def test_typed_settings():
+#     with mock_aws():
+
+#         class Settings(BaseModel):
+#             hostname: Setting
+#             password: Setting
+
+#     settings = AwsAppSettings(TEST_APP, TEST_ENV, settings_type=Settings)
+#     breakpoint()
